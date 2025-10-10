28ccdf16cb7b3099bf97f8edf90daa4fd459a1d2
Woodaengtang <jhwoo200@gmail.com>
2025-10-10 19:48:30 +0900

    Edit temp file

--- controller/tmp.py
+++ controller/tmp.py
@@ -16,17 +16,23 @@ app_launcher = AppLauncher(args_cli)
 simulation_app = app_launcher.app
 
 import torch
+
 import isaacsim.core.utils.prims as prim_utils
 import isaaclab.sim as sim_utils
-from isaaclab.assets import RigidObject, RigidObjectCfg
+
+from isaaclab.assets import Articulation, RigidObject, RigidObjectCfg
 from isaaclab.sim import SimulationContext
 from isaaclab.managers import SceneEntityCfg
+from isaaclab.utils.assets import ISAAC_NUCLEUS_DIR
+from isaaclab.markers.config import FRAME_MARKER_CFG, CUBOID_MARKER_CFG
+from isaaclab_assets import FRANKA_PANDA_CFG, FRANKA_PANDA_HIGH_PD_CFG
+from isaaclab.markers import VisualizationMarkersCfg, VisualizationMarkers
 from isaaclab.scene import InteractiveScene, InteractiveSceneCfg
-from isaaclab.assets import ArticulationCfg, Articulation, AssetBaseCfg
+from isaaclab.assets import ArticulationCfg, AssetBaseCfg
 from isaaclab.controllers import DifferentialIKController, DifferentialIKControllerCfg
 from isaaclab.controllers.joint_impedance import JointImpedanceController, JointImpedanceControllerCfg
 from isaaclab.utils import configclass
-from lib.env.GOAT_base_env_cfg import GOATBaseEnvCfg
+from isaaclab.utils.math import subtract_frame_transforms
 
 
 @configclass
@@ -41,6 +47,14 @@ class RobotSceneCfg(InteractiveSceneCfg):
     # lights
     dome_light = AssetBaseCfg(
         prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+    )
+    # table
+    table = AssetBaseCfg(
+        prim_path="{ENV_REGEX_NS}/Table",
+        spawn=sim_utils.UsdFileCfg(
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
+        ),
+        init_state=AssetBaseCfg.InitialStateCfg(pos=(0.0, 0.0, 1.05))
     )
     # robot
     # robot = FRANKA_PANDA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot",
@@ -82,26 +96,22 @@ class RobotSceneCfg(InteractiveSceneCfg):
     robot.actuators["panda_forearm"].damping = 0.0
 
 
-def run_simulator(sim : sim_utils.SimulationContext, env_cfg : GOATBaseEnvCfg):
-    # robot_entity_cfg = SceneEntityCfg("robot", joint_names=["panda_joint.*"], body_names=["panda_hand"])
-    # robot_entity_cfg.resolve(scene)
-    # robot = scene["robot"]
-
-    # define scene 
-    robot=Articulation(env_cfg.GOAT_cfg)
-    env_cfg.scene.articulations["robot"] = robot
-    scene=InteractiveScene(cfg=env_cfg.scene, sim=sim)
+def run_simulator(sim : sim_utils.SimulationContext, scene : InteractiveScene):
+    robot_entity_cfg = SceneEntityCfg("robot", joint_names=["panda_joint.*"], body_names=["panda_hand"])
+    robot_entity_cfg.resolve(scene)
+    robot = scene["robot"]
     sim_dt = sim.get_physics_dt()
     
     # 7개 조인트만 동작
-
-    # frame_marker_cfg = FRAME_MARKER_CFG.copy()
-    # frame_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
-    # left_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/left_finger"))
-    # left_finger_link_idx = robot.find_bodies("panda_leftfinger")[0][0]
-    # right_finger_link_idx = robot.find_bodies("panda_rightfinger")[0][0]
-    # left_finger_joint_idx = robot.find_joints("panda_finger_joint1")[0][0]
-    # right_finger_joint_idx = robot.find_joints("panda_finger_joint2")[0][0]
+    joint_ids = robot_entity_cfg.joint_ids
+    n_j = len(joint_ids)
+    frame_marker_cfg = FRAME_MARKER_CFG.copy()
+    frame_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
+    left_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/left_finger"))
+    left_finger_link_idx = robot.find_bodies("panda_leftfinger")[0][0]
+    right_finger_link_idx = robot.find_bodies("panda_rightfinger")[0][0]
+    left_finger_joint_idx = robot.find_joints("panda_finger_joint1")[0][0]
+    right_finger_joint_idx = robot.find_joints("panda_finger_joint2")[0][0]
 
     # Relative Impedance Controller
     joint_imp_cfg = JointImpedanceControllerCfg(command_type="p_rel", 
@@ -118,7 +128,7 @@ def run_simulator(sim : sim_utils.SimulationContext, env_cfg : GOATBaseEnvCfg):
                                                     device=scene.device)
 
     # ---------- 환경 준비 ----------
-    n_j = robot.num_joints()
+    n_j          = 7           # Franka: 9 (팔7+그리퍼2) 또는 7
     sim_len      = 2.0                        # [s] 실험 길이
     kp_table = torch.tensor([100, 300, 300, 300, 80, 80, 80], device=scene.device)
     zeta         = 0.3                       # Damping ratio(=가상 댐퍼 비율)
@@ -155,7 +165,7 @@ def run_simulator(sim : sim_utils.SimulationContext, env_cfg : GOATBaseEnvCfg):
                 # 강제로 조인트 워프 -> 초기 Configuration 재 설정
                 robot.write_joint_state_to_sim(default_joint_pos, default_joint_vel)
                 # 내부 버퍼에 토크 저장 -> 텔레포트가 아닌 실제 제어 입력을 위한 명령 신호
-                robot.set_joint_effort_target(zero_joint_efforts)
+                robot.set_joint_effort_target(zero_joint_efforts, joint_ids=joint_ids)
                 # 버퍼에 쓰인 전체 제어명령 전부 실행
                 robot.write_data_to_sim()
                 robot.reset()
@@ -188,7 +198,7 @@ def run_simulator(sim : sim_utils.SimulationContext, env_cfg : GOATBaseEnvCfg):
                 #### 만약, Direct로 Effort를 줄 생각이면, 액추에이터 쪽 PD제어 로직을 꺼야한다.
                 ####################################################################################
                 k = count % gripper_commands.shape[0]
-                robot.set_joint_effort_target(tau)
+                robot.set_joint_effort_target(tau, joint_ids=joint_ids)
                 robot.set_joint_position_target(gripper_commands[k].reshape(-1, 1).repeat(1, 2), joint_ids=[left_finger_joint_idx, right_finger_joint_idx])
                 print(f"gripper command : {gripper_commands[k]}")
 
@@ -387,9 +397,8 @@ def main():
     # Set main camera
     sim.set_camera_view([2.5, 2.5, 4.0], [0.0, 0.0, 0.0])
     # Design scene
-    scene_cfg = InteractiveScene(num_envs=args_cli.num_envs, env_spacing=2.0)
-    env_cfg = GOATBaseEnvCfg(scene=scene_cfg)
-    env_cfg.scene.articulations["robot"] = env_cfg.GOAT_cfg
+    scene_cfg = RobotSceneCfg(num_envs=args_cli.num_envs, env_spacing=2.0)
+    scene = InteractiveScene(scene_cfg)
     # Play the simulator
     sim.reset()
     # Now we are ready!
