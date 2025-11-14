e9523949c2638ef905e3e9de8db959e938659d04
OKsusu1411 <hansusu1411@gmail.com>
2025-11-14 20:43:01 +0900

    change screw axis

--- Realworld/torque_controller.py
+++ Realworld/torque_controller.py
@@ -13,12 +13,12 @@ class PDcontroller(Node):
         super().__init__('torque_controller')
 
         self.rotation_axis: np.array = np.array([                # Screw axies for each joints
+            [-1, 0, 0],
             [1, 0, 0],
-            [1, 0, 0],
+            [0, 1, 0],
             [0, 1, 0],
             [0, -1, 0],
             [0, -1, 0],
-            [0, 1, 0],
             [0, 1, 0],
             [0, 1, 0]
         ])
