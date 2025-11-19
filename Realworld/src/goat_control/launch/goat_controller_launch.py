5fc22bdc537b79499d748cc7e7a126cc5fa65a72
OKsusu1411 <hansusu1411@gmail.com>
2025-11-19 21:47:34 +0900

    edit launch file

--- Realworld/src/goat_control/launch/goat_controller_launch.py
+++ Realworld/src/goat_control/launch/goat_controller_launch.py
@@ -1,44 +1,54 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument, OpaqueFunction
+from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+
+def prepare_launch(context, *args, **kwargs):
+
+    urdf_path = LaunchConfiguration("model").perform(context)
+    with open(urdf_path, "r") as infp:
+        robot_description = infp.read()
+
+    return [
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name="robot_state_publisher",
+            parameters=[{"robot_description": robot_description}]
+        ),
+
+        # Node(                            # 이름 바꿔야됨
+        #     package='goat_control',
+        #     executable='states_pub',
+        #     name='states_pub'        
+        # ),
+
+        # Node(
+        #     package='goat_control',
+        #     executable='torque_converter',
+        #     name='torque_converter'
+        # ),
+
+        # Node(
+        #     package='goat_control',
+        #     executable='imu_publisher',
+        #     name='imu_publisher'
+        # )
+    ]
 
 def generate_launch_description():
 
-    # URDF file path
-    launch_file_dir = os.path.dirname(os.path.abspath(__file__))
-    goat_path = os.path.normpath(os.path.join(launch_file_dir, '..', '..', '..', '..', '..', '..'))
-    urdf_file = os.path.join(goat_path, 'lib/assets/GOAT/WF_GOAT/urdf', 'WF_GOAT.urdf')
-    
-    # Read URDF
-    with open(urdf_file, 'r') as f:
-        robot_description_content = f.read()
-    robot_description_param = {'robot_description': robot_description_content}
-
-    robot_state_publisher = Node(
-        package='robot_state_publisher',
-        executable='robot_state_publisher',
-        parameters=[robot_description_param]
-    )
-
-    states_pub = Node(                            # 이름 바꿔야됨
-        package='goat_control',
-        executable='states_pub'
-    )
-
-    torque_converter = Node(
-        package='goat_control',
-        executable='torque_converter'
-    )
-
-    imu_publisher = Node(
-        package='goat_control',
-        executable='imu_publisher'
-    )
+    goat_control_share_dir = get_package_share_directory('goat_control')
+    urdf_path = os.path.join(goat_control_share_dir, 'assets', 'WF_GOAT', 'urdf', 'WF_GOAT.urdf')
 
     return LaunchDescription([
-        robot_state_publisher,
-        states_pub,
-        torque_converter,
-        imu_publisher
+        DeclareLaunchArgument(
+            name="model",
+            default_value=urdf_path,
+            description="URDF file path"
+        ),
+
+        OpaqueFunction(function=prepare_launch)
     ])
