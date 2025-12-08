21bb5026f523cf0e7dcaa02053102f4b5b605ad6
Shiningsun00 <eojin333c@gmail.com>
2025-12-08 21:44:18 +0900

    fix : gain tunning

--- Realworld/src/goat_control/goat_control/pd_controller.py
+++ Realworld/src/goat_control/goat_control/pd_controller.py
@@ -61,8 +61,8 @@ DEFAULT_MAX_TORQUE = 4.5    # Maximum torque limit
 # DEFAULT_MAX_TORQUE_LIST   = [4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5, 4.5]
 
 # --- Per-joint default lists ---> rad --- hip = 30   thigh = 45
-DEFAULT_KP_LIST           = [0.15, 0.01, 0.27,   0.27,  0.0,   2.4, 0.0,    0.0]
-DEFAULT_KD_LIST           = [0.00,  0.0,  0.01,  0.001,  0.1,  0.0001,    0.0,    0.0]
+DEFAULT_KP_LIST           = [0.3, 0.01, 0.27,   0.27,  0.0,   2.4, 0.0,    0.0]
+DEFAULT_KD_LIST           = [0.01,  0.0,  0.01,  0.001,  0.1,  0.0001,    0.0,    0.0]
 DEFAULT_LPF_ALPHA_LIST    = [0.951,  0.951,   0.951,   0.951,  0.951,   0.951,  0.951,  0.951]
 DEFAULT_MAX_TORQUE_LIST   = [  4.5,    0.0,     0.0,     0.0,    0.0,    0.0,     0.0,    0.0]
 
