f4648517b1d10240ba57bc40cde866833df1a567
OKsusu1411 <hansusu1411@gmail.com>
2025-12-23 17:30:02 +0900

    RL let's go

--- Simulation/Tasks/GOAT_PD_stand/__init__.py
+++ Simulation/Tasks/GOAT_PD_stand/__init__.py
@@ -1,5 +1,5 @@
 import gymnasium as gym
-from .env.GOAT_PD_stand_env_cfg import GOATBaseEnvCfg
+# from .env.GOAT_PD_stand_env_cfg import GOATBaseEnvCfg
 
 gym.register(
     id="GOAT-stand-v0", 
@@ -8,9 +8,7 @@ gym.register(
     kwargs={
         # Environment-Specific Entry Point for Env Cfg Class
         "env_cfg_entry_point": f"{__name__}.GOAT_PD_stand_env_cfg:GOATPDStandEnvCfg",
-        "rl_ppo_cfg_entry_point": f"{GOATBaseEnvCfg.__name__}:ppo_cfg.yaml",
-        "rl_sac_cfg_entry_point": f"{GOATBaseEnvCfg.__name__}:sac_cfg.yaml",
-        "rl_td3_cfg_entry_point": f"{GOATBaseEnvCfg.__name__}:td3_cfg.yaml",
+        "rl_ppo_cfg_entry_point": f"{__name__}.cfg:rl_ppo_cfg.yaml",
     }
 )
 
