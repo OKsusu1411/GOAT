ab320a75dbaf2238395f2e38a29872ec4a3e0dc6
OKsusu1411 <hansusu1411@gmail.com>
2026-02-13 10:54:26 +0900

    commit editing

--- lib/env/GOAT_base_env_cfg.py
+++ lib/env/GOAT_base_env_cfg.py
@@ -104,8 +104,8 @@ GOAT_Cfg: ArticulationCfg = ArticulationCfg(
             stiffness=0.0,                          # Internal PD controller not used
             damping=0.0,                            # Internal PD controller not used
             friction=0.0033,                        # Static friction coefficient
-            dynamic_friction=0,          # Dynamic friction coefficient 
-            viscous_friction=0,          # Viscous friction coefficient
+            dynamic_friction=0,                     # Dynamic friction coefficient 
+            viscous_friction=0,                     # Viscous friction coefficient
         ),
 
         "thigh": DCMotorCfg(
