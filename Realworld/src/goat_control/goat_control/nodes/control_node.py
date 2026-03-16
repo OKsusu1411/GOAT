9cf9eb8ddd6c847855d04f0c20244afb58bc7c95
OKsusu1411 <hansusu1411@gmail.com>
2026-03-16 19:19:33 +0900

    release limit

--- Realworld/src/goat_control/goat_control/nodes/control_node.py
+++ Realworld/src/goat_control/goat_control/nodes/control_node.py
@@ -227,7 +227,7 @@ class GoatControlNode(Node):
 
 
         ###### ======================================= Safety stop ======================================= ###### 
-        has_joint_velocity_violation = (robot_state.joint_velocity_rad_per_sec[0:6] >= 0.436).any()
+        has_joint_velocity_violation = (robot_state.joint_velocity_rad_per_sec[0:6] >= 0.6).any()
 
         if has_joint_velocity_violation: #| self.has_position_limit_violation:
             self.get_logger().info("!!!!Emergency Stopped!!!!")
