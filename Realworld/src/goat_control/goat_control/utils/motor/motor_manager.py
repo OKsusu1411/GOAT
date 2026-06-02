1bc704754d6de58ba2c2029d39e588a5dc783a5d
Diligent_Yoon <grape43146@naver.com>
2026-06-02 13:40:30 +0900

    .

--- Realworld/src/goat_control/goat_control/utils/motor/motor_manager.py
+++ Realworld/src/goat_control/goat_control/utils/motor/motor_manager.py
@@ -367,7 +367,8 @@ class MotorManager:
         )
 
         if motor_index == 0:
-            print(f"Motor {motor_index}: current_count={current_count} | delta_count={delta_count} | wrapping_count={self.motor_encoder_wrap_count[motor_index]} | total_count_delta={total_count_delta}\r")
+            if abs(delta_count) > half_range:
+                print(f"Motor {motor_index}: current_count={current_count} | delta_count={delta_count} | wrapping_count={self.motor_encoder_wrap_count[motor_index]} | total_count_delta={total_count_delta}\r")
 
     # =========================================================================
     # [Read Only] 기존과 동일한 읽기 함수 (이제 공통 로직을 재사용합니다)
