769f39812a935acd6b49bfd601693c2806e24388
Diligent_Yoon <grape43146@naver.com>
2026-06-05 18:56:50 +0900

    good

--- Realworld/src/goat_control/launch/goat_control_system.launch.py
+++ Realworld/src/goat_control/launch/goat_control_system.launch.py
@@ -25,7 +25,7 @@ def generate_launch_description():
     default_checkpoint_path = PathJoinSubstitution([
         FindPackageShare("goat_control"),
         "checkpoint",
-        "agent_jit_128000.pt"
+        "agent_jit_56000.pt"
     ])
 
     # Arguments
