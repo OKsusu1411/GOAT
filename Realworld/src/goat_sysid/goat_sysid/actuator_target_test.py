f964eecf93de7ff3041ed1a086160569f985374e
OKsusu1411 <hansusu1411@gmail.com>
2026-08-08 15:43:24 +0900

    zero torque

--- Realworld/src/goat_sysid/goat_sysid/actuator_target_test.py
+++ Realworld/src/goat_sysid/goat_sysid/actuator_target_test.py
@@ -238,6 +238,11 @@ class ActuatorTargetTestNode(Node):
             elif key == 'q':
                 self.logger.info("Shutting down Agent Node...\r")
                 self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
+                self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
+                self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
+                self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
+                self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
+                self.motor_io.read_write_motor(np.zeros(self.num_joints, dtype=np.float32))
                 rclpy.shutdown()
                 break
             else:
