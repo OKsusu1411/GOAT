1b3f1482224dd1e2f217245a18c040f5101a2815
OKsusu1411 <hansusu1411@gmail.com>
2026-08-23 17:10:02 +0900

    .

--- Realworld/src/goat_control/goat_control/nodes/controller_node.py
+++ Realworld/src/goat_control/goat_control/nodes/controller_node.py
@@ -413,7 +413,7 @@ class ControllerNode(Node):
             return
 
         # Publish torque command (only start mode)
-        tau[:] = 0
+        tau[:] = safe_torque
         q_current = self.motor_io.read_write_motor(tau)                                     
 
         # Publish for logging
