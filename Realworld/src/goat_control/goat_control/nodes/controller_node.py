b029fed0fd3bf21780351050eb70c31f7cd38ccc
Diligent_Yoon <grape43146@naver.com>
2026-09-10 11:50:12 +0900

    .

--- Realworld/src/goat_control/goat_control/nodes/controller_node.py
+++ Realworld/src/goat_control/goat_control/nodes/controller_node.py
@@ -447,11 +447,13 @@ class ControllerNode(Node):
                 f"[CAN] write_wait={self.motor_io.motor_manager._last_write_wait_ms:.3f}\r"
             )
 
-            for i in range(self.motor_io.motor_manager.motor_count):
-                self.logger.info(
-                    f"[CAN] motor{i} request start={self.motor_io.motor_manager.motor_request_start_time_ms[i]:.3f} | "
-                    f"motor{i} request end={self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f}\r"
-                )
+            motor_timing_str = " | ".join(
+                f"M{i}: "
+                f"{self.motor_io.motor_manager.motor_request_start_time_ms[i]:.3f}"
+                f"->{self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f}"
+                for i in range(self.motor_io.motor_manager.motor_count)
+            )
+            self.logger.info(f"[CAN] Motor timings: {motor_timing_str}")
 
             self._rate_window_start = now
             self._rate_cycle_count = 0
