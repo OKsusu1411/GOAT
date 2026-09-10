be0c16318b7f9a76930b317ab72181b1ef3a584f
Diligent_Yoon <grape43146@naver.com>
2026-09-10 12:27:06 +0900

    .

--- Realworld/src/goat_control/goat_control/nodes/controller_node.py
+++ Realworld/src/goat_control/goat_control/nodes/controller_node.py
@@ -450,7 +450,7 @@ class ControllerNode(Node):
             motor_timing_str = " | ".join(
                 f"M{i}: "
                 f"{self.motor_io.motor_manager.motor_request_start_time_ms[i]:.3f}"
-                f"->{self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f}"
+                f"->{self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f} "
                 f"Wait={self.motor_io.motor_manager.motor_wait_time_ms[i]:.3f}"
                 for i in range(self.motor_io.motor_manager.motor_count)
             )
