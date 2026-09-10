3eb2eb038d83e56935cb5b1fb327237fde9cc75d
Diligent_Yoon <grape43146@naver.com>
2026-09-10 13:39:28 +0900

    torque test

--- Realworld/src/goat_control/goat_control/nodes/controller_node.py
+++ Realworld/src/goat_control/goat_control/nodes/controller_node.py
@@ -447,14 +447,14 @@ class ControllerNode(Node):
                 f"[CAN] write_wait={self.motor_io.motor_manager._last_write_wait_ms:.3f}\r"
             )
 
-            motor_timing_str = " | ".join(
-                f"M{i}: "
-                f"{self.motor_io.motor_manager.motor_request_start_time_ms[i]:.3f}"
-                f"->{self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f} "
-                f"Wait={self.motor_io.motor_manager.motor_wait_time_ms[i]:.3f}"
-                for i in range(self.motor_io.motor_manager.motor_count)
-            )
-            self.logger.info(f"[CAN] Motor timings: {motor_timing_str}\r")
+            # motor_timing_str = " | ".join(
+            #     f"M{i}: "
+            #     f"{self.motor_io.motor_manager.motor_request_start_time_ms[i]:.3f}"
+            #     f"->{self.motor_io.motor_manager.motor_request_end_time_ms[i]:.3f} "
+            #     f"Wait={self.motor_io.motor_manager.motor_wait_time_ms[i]:.3f}"
+            #     for i in range(self.motor_io.motor_manager.motor_count)
+            # )
+            # self.logger.info(f"[CAN] Motor timings: {motor_timing_str}\r")
 
             self._rate_window_start = now
             self._rate_cycle_count = 0
