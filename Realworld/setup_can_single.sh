8b1f4699342ff27a82edf2c1b4c6a48a34eb947e
OKsusu1411 <hansusu1411@gmail.com>
2026-09-14 16:04:01 +0900

    almost

--- Realworld/setup_can_single.sh
+++ Realworld/setup_can_single.sh
@@ -4,9 +4,8 @@ set -e
 
 IFACE="${1:-can0}"
 BITRATE="${2:-1000000}"
-DBITRATE="${3:-5000000}"
 
-echo "[*] Setting up CAN interface: ${IFACE} @ ${BITRATE} bps, FD ${DBITRATE} bps"
+echo "[*] Setting up CAN interface: ${IFACE} @ ${BITRATE} bps"
 
 # Native Jetson CAN driver
 sudo modprobe can
@@ -26,10 +25,8 @@ sudo ip link set "${IFACE}" down || true
 echo "[*] Configuring ${IFACE}..."
 sudo ip link set "${IFACE}" up type can \
     bitrate "${BITRATE}" \
-    dbitrate "${DBITRATE}"\
     restart-ms 100 \
     berr-reporting on \
-    fd on
 
 sleep 0.5
 
@@ -40,7 +37,7 @@ ip -details -statistics link show "${IFACE}"
 echo
 
 if ip link show "${IFACE}" | grep -q "UP"; then
-    echo "[OK] ${IFACE} is UP and configured at ${BITRATE} bps, FD ${DBITRATE} bps."
+    echo "[OK] ${IFACE} is UP and configured at ${BITRATE} bps" 
 else
     echo "[FAIL] ${IFACE} is NOT UP."
     exit 1
